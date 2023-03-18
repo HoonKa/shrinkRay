0a5da5cf10d34c09296dfdfb96b41f09138d46f9
HoonKa <younghoo.kang@smail.astate.edu>
2023-03-18 18:51:45 +0900

    express session update

--- src/types/express-session.d.ts
+++ src/types/express-session.d.ts
@@ -4,6 +4,13 @@ declare module 'express-session' {
   export interface Session {
     clearSession(): Promise<void>; // DO NOT MODIFY THIS!
 
+    authenticatedUser: {
+      userId: string;
+      isPro: true;
+      isAdmin: true;
+      username: string;
+    };
+
     // NOTES: Add your app's custom session properties here:
   }
 }
